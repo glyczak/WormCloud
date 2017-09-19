be6df1cb1b1e44b3c5e3729889cc60f509ba3fad
Gabe Lyczak <glyczak@gmail.com>
2017-09-19 15:02:33 -0400

    Added client side verification

--- WormCloud/App_Start/BundleConfig.cs
+++ WormCloud/App_Start/BundleConfig.cs
@@ -25,14 +25,17 @@ namespace WormCloud
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-spacelab.css",
-                      "~/Content/site.css",
-                      "~/Content/DataTables/css/datatables.bootstrap.css"
+                      "~/Content/DataTables/css/datatables.bootstrap.css",
+                      "~/Content/Site.css"
                       ));
         }
     }
